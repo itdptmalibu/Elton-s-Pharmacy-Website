c8986cefbaff7032902646933335582a1400754d
Mutoria <johnngugi7402@gmail.com>
2026-03-13 08:38:20 +0300

    smaller images and more responsive layout for the phone

--- script.js
+++ script.js
@@ -88,5 +88,10 @@ window.addEventListener('scroll', () => {
     }
   });
 });
+const menuToggle = document.getElementById("menuToggle");
+const navMenu = document.getElementById("navMenu");
 
+menuToggle.addEventListener("click", () => {
+navMenu.classList.toggle("active");
+});
 
